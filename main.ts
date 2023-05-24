d103dee35e3c321787667d47cebb314d2c8a1cff
William Faircloth <willuhmjs@gmail.com>
2023-05-24 20:42:05 +0000

    log isip

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import { ConnInfo, Handler, serve } from "https://deno.land/std@0.187.0/http/server.ts";
+import isbot from "npm:isbot";
 
 const handler: Handler = (req: Request, connInfo: ConnInfo): Response => {
   const headers = req.headers;
   const ip = headers.get("x-real-ip") || headers.get("x-forwarded-for") || (connInfo.remoteAddr as Deno.NetAddr).hostname;
-  console.log(ip);
-  console.log(headers.get("user-agent"));
+  console.log(`isbot: ${isbot(req.headers.get("user-agent"))}, ip: ${ip}`);
   return new Response(null, {
     status: 302,
     headers: new Headers({
